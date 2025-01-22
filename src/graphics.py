47b23c8ea368f696b7554b0a40c0d8a9021e06bc
Jake Harrison <jake.hsh100@gmail.com>
2025-01-22 11:16:27 +0000

    removed grid

--- src/graphics.py
+++ src/graphics.py
@@ -10,21 +10,15 @@ bg = pygame.transform.rotate(bg, 90)
 shirt=pygame.transform.scale(pygame.image.load("assets/shirt/shirt.png"), (50, 50))
 shirt_locations = [(144,108), (72,180), (216,180), (144,612), (144,324), (72,360), (216,360), (36,504), (252,504), (108,540), (180,540)]
 
-def draw_grid():
-    for i in range(0, 324, 36):
-        pygame.draw.line(screen, (255, 255, 255), (i, 0), (i, 720))
-    for j in range(0, 720, 36):
-        pygame.draw.line(screen, (255, 255, 255), (0, j), (324, j))
+
 
 run = True
 while run == True:
     screen.blit(bg, (0, 0))
     for i in range(len(shirt_locations)):
         screen.blit(shirt, (shirt_locations[i][0] - 7, shirt_locations[i][1] - 7))
-    draw_grid()
     
-
-    draw_grid()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
