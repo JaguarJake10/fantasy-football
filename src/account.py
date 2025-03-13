d4d967fb87d2df58f3a7084cc89544a0df4ba3dd
Jake Harrison <jake.hsh100@gmail.com>
2025-03-13 12:19:57 +0000

    Added menus

--- src/account.py
+++ src/account.py
@@ -6,6 +6,7 @@ class Account:
         self.screenNo = 0
         self.screen = screen
         self.bg = pygame.transform.scale(pygame.image.load("assets/image/football.png"), (324, 720))
+        pygame.display.set_caption("Fantasy Football")
 
     def screenNum(self):
         print("I am working")
@@ -18,6 +19,9 @@ class Account:
         elif self.screenNo == 2:
             print("I work")
             self.screenNo = self.signup()
+            self.screenNum()
+        elif self.screenNo == 3:
+            self.screenNo == self.signupSelect()
             self.screenNum()
         elif self.screenNo == -1:
             pygame.quit()
@@ -34,7 +38,7 @@ class Account:
         login = pygame.font.Font("assets/font/font.ttf", 25).render("Login", True, (0, 0, 0))
         titleTop = pygame.font.Font("assets/font/font.ttf", 25).render("Fantasy", True, (0, 0, 0))
         titleBottom = pygame.font.Font("assets/font/font.ttf", 25).render("Football", True, (0, 0, 0))
-        ball = pygame.transform.scale(pygame.image.load("assets/image/ball.png"), (70, 70))
+        ball = pygame.transform.scale(pygame.image.load("assets/image/newball.png"), (200, 200))
         while onMenu:
             self.screen.fill((0, 158, 5))
             pos = pygame.mouse.get_pos()
@@ -55,7 +59,7 @@ class Account:
             self.screen.blit(login, (90, 525))
             self.screen.blit(titleTop, (45,100))
             self.screen.blit(titleBottom, (50, 150))
-            self.screen.blit(ball, (100, 200))
+            self.screen.blit(ball, (60, 175))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     onMenu = False
@@ -70,29 +74,166 @@ class Account:
 
     def signup(self):
         onSignup = True
+        user_text3 = ''
+        user_text4 = ''
+        text3Selected = False
+        text4Selected = False
+        next = pygame.font.Font("assets/font/font.ttf", 25).render("Next", True, (0, 0, 0))
+        button3Hover = False
         while onSignup:
             self.screen.fill((0, 158, 5))
             pos = pygame.mouse.get_pos()
+            if not (30 <= pos[0] <= 294 and 635 <= pos[1] <= 685):
+                pygame.draw.rect(self.screen, (255, 255, 255), (30, 635, 264, 50), 5)
+                button3Hover = False
+            else:
+                pygame.draw.rect(self.screen, (255, 255, 255), (30, 635, 264, 50))
+                button3Hover = True
+            clock = pygame.time.Clock()
+            base_font = pygame.font.Font("assets/font/font.ttf", 12)
+            input_rect2 = pygame.Rect(20,200,285,42)
+            input_rect3 = pygame.Rect(20,300,285,42)
+            color_active = pygame.Color('black')
+            color_passive = pygame.Color('black')
+            colour = color_passive
+            selected = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    if 20 <= pos[0] <= 305 and 200 <= pos[1] <= 242:
+                        text3Selected = True
+                        text4Selected = False
+                    elif 20 <= pos[0] <= 305 and 300 <= pos[1] <= 342:
+                        text3Selected = False
+                        text4Selected = True
+                    else:
+                        text3Selected = False
+                        text4Selected = False
+                    print("123")
+                    if button3Hover:
+                        print("Yes")
+                        return 3
+                else:
+                    selected = False
+                
+                if event.type == pygame.KEYDOWN:
+                    if text3Selected:
+                        if event.key == pygame.K_BACKSPACE:
+                            user_text3 = user_text3[:-1]
+                        else:
+                            user_text3 += event.unicode
+                    elif text4Selected:
+                        if event.key == pygame.K_BACKSPACE:
+                            user_text4 = user_text4[:-1]
+                        else:
+                            user_text4 += event.unicode
+            
+            if selected:
+                colour = color_active
+            else:
+                colour = color_passive
+            
+            pygame.draw.rect(self.screen, colour, input_rect2)
+            pygame.draw.rect(self.screen, colour, input_rect3)
+            text_surface = base_font.render(user_text3, True, (255, 255, 255))
+            text_surface1 = base_font.render(user_text4, True, (255, 255, 255))
+            self.screen.blit(text_surface, (input_rect2.x+5, input_rect2.y+5))
+            self.screen.blit(text_surface1, (input_rect3.x+5, input_rect3.y+5))
+            self.screen.blit(next, (95, 650))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     onSignup = False
                     return -1
+                #elif event.type == pygame.MOUSEBUTTONDOWN:
+                    
+                        
+            clock.tick(60)
             pygame.display.flip()
 
 
 
     def login(self):
         onLogin = True
+        text1Selected = False
+        text2Selected = False
         while onLogin:
             self.screen.fill((0, 158, 5))
-            pos = pygame.mouse.get_pos()
+            clock = pygame.time.Clock()
+            base_font = pygame.font.Font("assets/font/font.ttf", 12)
+            user_text1 = ''
+            user_text2 = ''
+            input_rect = pygame.Rect(20,200,285,42)
+            input_rect1 = pygame.Rect(20,300,285,42)
+            color_active = pygame.Color('black')
+            color_passive = pygame.Color('black')
+            colour = color_passive
+            selected = False
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        pos = pygame.mouse.get_pos()
+                        if 20 <= pos[0] <= 305 and 200 <= pos[1] <= 242:
+                            text1Selected = True
+                            text2Selected = False
+                        elif 20 <= pos[0] <= 305 and 300 <= pos[1] <= 342:
+                            text1Selected = False
+                            text2Selected = True
+                        else:
+                            text1Selected = False
+                            text2Selected = False
+                    else:
+                        selected = False
+                    
+                    if event.type == pygame.KEYDOWN:
+                        if text1Selected:
+                            if event.key == pygame.K_BACKSPACE:
+                                user_text1 = user_text1[:-1]
+                            else:
+                                if len(user_text1) < 15:
+                                    user_text1 += event.unicode
+                        elif text2Selected:
+                            if event.key == pygame.K_BACKSPACE:
+                                user_text2 = user_text2[:-1]
+                            else:
+                                if len(user_text2) < 15:
+                                    user_text2 += event.unicode
+                
+                if selected:
+                    colour = color_active
+                else:
+                    colour = color_passive
+                
+                pygame.draw.rect(self.screen, colour, input_rect)
+                pygame.draw.rect(self.screen, colour, input_rect1)
+                text_surface = base_font.render(user_text1, True, (255, 255, 255))
+                text_surface1 = base_font.render(user_text2, True, (255, 255, 255))
+                self.screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+                self.screen.blit(text_surface1, (input_rect1.x+5, input_rect1.y+5))
+                clock.tick(60)
+                pygame.display.flip()
+
+    def signupSelect(self):
+        print("Gotem")
+        onSelect = True
+        teamSelect = pygame.font.Font("assets/font/font.ttf", 25).render("Select a team", True, (0, 0, 0))
+        self.screen.blit(teamSelect, (0,0))
+        while onSelect:
+            self.screen.fill((0, 158, 5))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    onLogin = False
-                    return -1
+                    onSelect = False
+                    pygame.quit()
+                    sys.exit()
             pygame.display.flip()
+
+
 
 
 account = Account(pygame.display.set_mode((324, 720)))
 account.screenNum()
-    
