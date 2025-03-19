c73c652e30a41fbcdb344a533db37646a944ab73
Jake Harrison <jake.hsh100@gmail.com>
2025-03-19 14:28:39 +0000

    added images

--- src/account.py
+++ src/account.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from button import *
 pygame.init()
 
 class Account:
@@ -7,6 +8,7 @@ class Account:
         self.screen = screen
         self.bg = pygame.transform.scale(pygame.image.load("assets/image/football.png"), (324, 720))
         pygame.display.set_caption("Fantasy Football")
+        pygame.display.set_icon(pygame.image.load("assets/image/newball.png"))
 
     def screenNum(self):
         print("I am working")
@@ -219,12 +221,53 @@ class Account:
                 pygame.display.flip()
 
     def signupSelect(self):
-        print("Gotem")
         onSelect = True
-        teamSelect = pygame.font.Font("assets/font/font.ttf", 25).render("Select a team", True, (0, 0, 0))
-        self.screen.blit(teamSelect, (0,0))
+        teamSelect = pygame.font.Font("assets/font/font.ttf", 15).render("Select a team", True, (0, 0, 0))
+        bournemouth = Button(self.screen, "Bournemouth", pygame.transform.scale(pygame.image.load("assets/image/Bournemouth.png"), (200, 100), self.username))
+        bournemouth.draw()
+        bournemouth = pygame.transform.scale(pygame.image.load("assets/image/Bournemouth.png"), (200, 100))
+        arsenal = pygame.transform.scale(pygame.image.load("assets/image/Arsenal.png"), (100, 100))
+        astonVilla = pygame.transform.scale(pygame.image.load("assets/image/aston villa.png"), (200, 100))
+        brentford = pygame.transform.scale(pygame.image.load("assets/image/brentford.png"), (200, 100))
+        brighton = pygame.transform.scale(pygame.image.load("assets/image/brighton.png"), (200, 100))
+        chelsea = pygame.transform.scale(pygame.image.load("assets/image/chelsea.png"), (200, 100))
+        crystalPalace = pygame.transform.scale(pygame.image.load("assets/image/crystal palace.png"), (200, 100))
+        everton = pygame.transform.scale(pygame.image.load("assets/image/everton.png"), (200, 100))
+        fulham = pygame.transform.scale(pygame.image.load("assets/image/fulham.png"), (200, 100))
+        ipswichTown = pygame.transform.scale(pygame.image.load("assets/image/ipswich town.png"), (200, 100))
+        leicester = pygame.transform.scale(pygame.image.load("assets/image/leicester city.png"), (200, 100))
+        liverpool = pygame.transform.scale(pygame.image.load("assets/image/liverpool.png"), (200, 100))
+        manCity = pygame.transform.scale(pygame.image.load("assets/image/man city.png"), (200, 100))
+        manUnited = pygame.transform.scale(pygame.image.load("assets/image/man united.png"), (200, 100))
+        newcastle = pygame.transform.scale(pygame.image.load("assets/image/newcastle.png"), (200, 100))
+        forest = pygame.transform.scale(pygame.image.load("assets/image/nottingham forest.png"), (200, 100))
+        southampton = pygame.transform.scale(pygame.image.load("assets/image/southampton.png"), (200, 100))
+        tottenham = pygame.transform.scale(pygame.image.load("assets/image/tottenham.png"), (200, 100))
+        westHam = pygame.transform.scale(pygame.image.load("assets/image/west ham.png"), (200, 100))
+        wolves = pygame.transform.scale(pygame.image.load("assets/image/wolves.png"), (200, 100))
         while onSelect:
             self.screen.fill((0, 158, 5))
+            self.screen.blit(teamSelect, (45,25))
+            self.screen.blit(bournemouth, (0, 100))
+            self.screen.blit(arsenal, (150, 100))
+            self.screen.blit(astonVilla, (0, 200))
+            self.screen.blit(brentford, (150, 200))
+            self.screen.blit(brighton, (0, 300))
+            self.screen.blit(chelsea, (150, 300))
+            self.screen.blit(crystalPalace, (0, 400))
+            self.screen.blit(everton, (150, 400))
+            self.screen.blit(fulham, (0, 500))
+            self.screen.blit(ipswichTown, (150, 500))
+            self.screen.blit(leicester, (0, 600))
+            self.screen.blit(liverpool, (150, 600))
+            self.screen.blit(manCity, (0, 650))
+            self.screen.blit(manUnited, (150, 650))
+            self.screen.blit(newcastle, (0, 700))
+            self.screen.blit(forest, (150, 700))
+            self.screen.blit(southampton, (0, 500))
+            self.screen.blit(tottenham, (0, 500))
+            self.screen.blit(westHam, (0, 500))
+            self.screen.blit(wolves, (0, 500))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     onSelect = False
